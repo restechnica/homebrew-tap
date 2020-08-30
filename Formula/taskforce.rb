147dd9fbebcad7c901bdb26979bf040db7114574
Olivier Van Aken <olivier.va@outlook.com>
2020-08-31 00:36:26 +0200

    added taskforce formula for release v0.0.2

---
+++ Formula/taskforce.rb
@@ -0,0 +1,15 @@
+class Taskforce < Formula
+  desc "Tool to build, change, and version operations"
+  homepage "https://github.com/restechnica/taskforce"
+  license "MPL-2.0"
+  url "https://github.com/restechnica/taskforce/archive/v0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "75ddd2c32ae8ed15412f8cce127776b9062b683d1fe55f66a8f6adf8622a020b"
+
+  depends_on "go@1.14" => :build
+
+  def install
+    system "make", "build"
+    bin.install "bin/taskforce"
+  end
+end
