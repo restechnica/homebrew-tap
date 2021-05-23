e18d38242957c952f3ff5f20cd5b8210bbc2c1b6
Olivier Van Aken <olivier.va@outlook.com>
2021-05-23 19:03:27 +0200

    upgraded semverbot to 0.0.3

--- Formula/semverbot.rb
+++ Formula/semverbot.rb
@@ -1,4 +1,4 @@
-VERSION = "x.x.x"
+VERSION = "0.0.3"
 
 class Semverbot < Formula
   desc "A CLI which automates semver versioning."
@@ -6,7 +6,7 @@ class Semverbot < Formula
   license "MPL-2.0"
   url "https://github.com/restechnica/semverbot/archive/v#{VERSION}.tar.gz"
   version VERSION
-  sha256 "?"
+  sha256 "42DE3817E14D22AB57E7F4599CAFC7E477EC279FF22227F48E0883C022E67630"
 
   depends_on "go" => :build
 
