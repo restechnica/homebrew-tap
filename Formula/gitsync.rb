5d1f5e1c41cb79fffb0128d302660dc5edc5b8c7
Olivier Van Aken <olivier.va@outlook.com>
2023-06-12 02:42:56 +0200

    updated github urls

--- Formula/gitsync.rb
+++ Formula/gitsync.rb
@@ -4,7 +4,7 @@ class Gitsync < Formula
   desc "A CLI to sync git repositories."
   homepage "https://github.com/restechnica/gitsync-cli"
   license "MPL-2.0"
-  url "https://github.com/restechnica/gitsync-cli/archive/v#{VERSION}.tar.gz"
+  url "https://github.com/restechnica/semverbot/archive/refs/tags/v#{VERSION}.tar.gz"
   version VERSION
   sha256 "d13a229c86a35978548e19aa2dc1f311373a591fe20572370bcf320d9ba4f953" # on windows use `CertUtil -hashfile <file location> SHA256`
 
--- Formula/semverbot.rb
+++ Formula/semverbot.rb
@@ -4,7 +4,7 @@ class Semverbot < Formula
   desc "A CLI which automates semver versioning."
   homepage "https://github.com/restechnica/semverbot"
   license "MPL-2.0"
-  url "https://github.com/restechnica/semverbot/archive/v#{VERSION}.tar.gz"
+  url "https://github.com/restechnica/semverbot/archive/refs/tags/v#{VERSION}.tar.gz"
   version VERSION
   sha256 "6739413b22fc98e2c32fcff3c4f6bd0ad2e50827b24b01b8cbeed8a2fede72b2" # on windows use `CertUtil -hashfile <file location> SHA256`
 
