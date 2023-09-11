cfad85a5423d87eb6a2617bd4013cada15b8b999
Olivier Van Aken <olivier.va@outlook.com>
2023-09-11 19:11:14 +0200

    updated to semverbot 1.3.2

--- Formula/semverbot.rb
+++ Formula/semverbot.rb
@@ -1,4 +1,4 @@
-VERSION = "1.3.1"
+VERSION = "1.3.2"
 
 class Semverbot < Formula
   desc "A CLI which automates semver versioning."
@@ -6,7 +6,7 @@ class Semverbot < Formula
   license "MPL-2.0"
   url "https://github.com/restechnica/semverbot/archive/refs/tags/v#{VERSION}.tar.gz"
   version VERSION
-  sha256 "6739413b22fc98e2c32fcff3c4f6bd0ad2e50827b24b01b8cbeed8a2fede72b2" # on windows use `CertUtil -hashfile <file location> SHA256`
+  sha256 "6371f780fafa44982f421d4359b46342e6ecb31c93b28cbe7b895832439aadbb" # on windows use `CertUtil -hashfile <file location> SHA256`
 
   depends_on "go" => :build
 
