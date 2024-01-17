d602fef56cb40c4bf5c88c691e5b93f6ec63d6f9
Olivier Van Aken <olivier.va@outlook.com>
2024-01-17 16:51:06 +0100

    upgraded from semverbot 1.4.1 to 1.6.2 - added nushell as building dependency for sbot - added version as a parameter to the install script

--- Formula/semverbot.rb
+++ Formula/semverbot.rb
@@ -1,4 +1,4 @@
-VERSION = "1.4.1"
+VERSION = "1.6.2"
 
 class Semverbot < Formula
   desc "A CLI which automates semver versioning."
@@ -6,12 +6,15 @@ class Semverbot < Formula
   license "MPL-2.0"
   url "https://github.com/restechnica/semverbot/archive/refs/tags/v#{VERSION}.tar.gz"
   version VERSION
-  sha256 "15268f5d87d6f86e60b1d95f12ea2a06e43fd11a7f120016461239723534887d" # on windows use `CertUtil -hashfile <file location> SHA256`
+  # on windows use `CertUtil -hashfile <file location> SHA256`
+  # on macos: shasum -a 256 /path/to/file
+  sha256 "fa23bce6d2fa0aa35203f9263a025aebc46c40433003f0360ddc4cb4a4554c02"
 
   depends_on "go" => :build
+  depends_on "nushell" => :build
 
   def install
-    system "make", "simple-build"
+    system "nu", "run.nu", "build", "--version", VERSION
     bin.install "bin/sbot"
   end
 end
